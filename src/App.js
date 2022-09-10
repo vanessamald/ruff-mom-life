7163353712611cfd13a33842ec97854f8106a17c
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-09-10 12:03:58 -0500

    integrating snipcart and styling product cards with chakra ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import {
+  Container,
+  Text,
+  Box,
+  Divider,
+  Image,
+  Button,
+  Flex,
+  SimpleGrid
+} from "@chakra-ui/react";
+import { FiShoppingCart, FiShoppingBag } from "react-icons/fi";
 import Butter from 'buttercms';
 
 const butter = Butter(process.env.REACT_APP_BUTTER_ECOMMERCE);
@@ -23,16 +34,118 @@ function App() {
   
   return (
     
-  <div>
-    {products.map((product) => (
-      <div>
-        <img src={product.image} alt={`${product.name}`} />
-        {product.name}
-        {product.price}
-        {product.description}
-      </div>
-    ))}
-  </div>
+<Container maxW="container.xl" h="100vh" >
+      <Flex justifyContent="space-between" alignContent="center">
+        <Text
+          as="a"
+          href="/"
+          fontSize="2rem"
+          color="gray.900"
+          fontFamily="Robo"
+          my="5px"
+        >
+          Ruff Mom Life
+        </Text>
+        <Button
+          my="5px"
+          colorScheme="green"
+          variant="ghost"
+          leftIcon={<FiShoppingBag size="24px" />}
+          size="lg"
+          p={2}
+
+          className="snipcart-checkout"
+        >
+          View Cart
+        </Button>
+      </Flex>
+      <Divider />
+      <Box mt={4}>
+        <SimpleGrid
+          minChildWidth="300px"
+          align="center"
+          justify="center"
+          spacing="40px"
+          mb={32}
+        >
+          {products.map((product) => (
+            <Box
+              bg="white"
+              maxW="sm"
+              borderWidth="1px"
+              rounded="lg"
+              shadow="lg"
+              _hover={{ shadow: "dark-lg" }}
+              key={product.id}
+            >
+              <Image
+                h="350px"
+                fit="cover"
+                src={product.image}
+                alt={`Picture of ${product.name}`}
+                roundedTop="lg"
+              />
+
+              <Box p="6">
+                <Flex
+                  mt="1"
+                  justifyContent="space-between"
+                  alignContent="center"
+                >
+                  <Text
+                    fontSize="2xl"
+                    fontWeight="semibold"
+                    as="h4"
+                    textTransform="uppercase"
+                    lineHeight="tight"
+                    fontFamily="Roboto"
+                  >
+                    {product.name}
+                  </Text>
+                  <Text
+                    as="h4"
+                    fontSize="2xl"
+                    fontWeight="bold"
+                    color="teal.600"
+                  >
+                    ${product.price}
+                  </Text>
+                </Flex>
+
+                <Text
+                  mt={2}
+                  color="gray.500"
+                  display={{ base: "none", md: "flex" }}
+                >
+                  {product.description}
+                </Text>
+
+                <Button
+                  leftIcon={<FiShoppingCart size="24px" />}
+                  size="lg"
+                  mt={4}
+                  isFullWidth
+                  colorScheme="blue"
+                  variant="outline"
+                  alignSelf={"center"}
+
+                  className="snipcart-add-item"
+                  data-item-id={product.id}
+                  data-item-image={product.image}
+                  data-item-name={product.name}
+                  data-item-url="/"
+                  data-item-description={product.description}
+                  data-item-price={product.price}
+                >
+                  Add to Cart
+                </Button>
+              </Box>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Box>
+    </Container>
+
   );
 }
 
