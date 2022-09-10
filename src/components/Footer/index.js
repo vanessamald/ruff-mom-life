89eb302b9dccd706f8d45ecbcadc7ffb33bdc870
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-09-10 12:25:26 -0500

    refactoring code, adding components and styling

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div
+        style= {{
+            fontSize: '16px',
+            textAlign: 'center',
+            marginBottom: '0px',
+            position: 'relative',
+        }}>
+        <p>Made with love by Vanessa Maldonado</p>
+        </div>
+    );
+    }
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div
+    style= {{
+        padding: '0 20px',
+        height: '60px',
+        fontSize: '54px',
+        textAlign: 'center',
+        marginBottom: '20px',
+    }}>
+      <h1>Ruff Mom Life</h1>
+    </div>
+  );
+}
+
+export default Header;
